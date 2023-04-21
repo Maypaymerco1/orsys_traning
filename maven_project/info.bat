98b50835cb3a96d5eac46d54e2d4f732bd972c19
Maypaymerco1 <53341528+Maypaymerco1@users.noreply.github.com>
2023-04-21 11:39:21 +0200

    Delete info.bat

--- maven_project/info.bat
+++
@@ -1,6 +0,0 @@
-echo off 
-mvn archetype:generate -DgroupId=com.mm.training -DartifactId=testbat -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
-cd project_name 
-mvn clean package 
-cd target/classes 
-java com.app.training.App
